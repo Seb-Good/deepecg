ada016a1967669d6fbba9ea85cb12d5ff1562c21
Sebastian Goodfellow <sebi.goodfellow@gmail.com>
2018-12-01 13:52:10 -0500

    - Folder restructure.

---
+++ deepecg/config/config.py
@@ -0,0 +1,23 @@
+"""
+config.py
+---------
+Config variables for the project.
+By: Sebastian D. Goodfellow, Ph.D., 2018
+"""
+
+# Compatibility imports
+from __future__ import absolute_import, division, print_function
+
+# Local imports
+import os
+
+# Set working directory
+WORKING_DIR = (
+    os.path.dirname(
+        os.path.dirname(
+            os.path.dirname(
+                os.path.realpath(__file__)
+            )
+        )
+    )
+)
